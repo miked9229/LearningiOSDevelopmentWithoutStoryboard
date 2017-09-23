dcdac13a6078cc7b335b27bdcf1d5dff55305810
Michael Doroff <michael_doroff@yahoo.com>
2017-09-23 14:47:43 -0500

    Getting rid of Storyboard

--- CodingWithoutStoryboard/AppDelegate.swift
+++ CodingWithoutStoryboard/AppDelegate.swift
@@ -16,7 +16,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let homeController = HomeController(collectionViewLayout: UICollectionViewLayout())
+        
+        window?.rootViewController = UINavigationController(rootViewController: homeController)
+        
         return true
+   
+    
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
---
+++ CodingWithoutStoryboard/HomeController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeController.swift
+//  CodingWithoutStoryboard
+//
+//  Created by Michael Doroff on 9/23/17.
+//  Copyright © 2017 Michael Doroff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .green
+    }
+    
+    
+    
+}
