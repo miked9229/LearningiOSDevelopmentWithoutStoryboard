83ca06cc360b580dcf870a3f202fb81f7e3c7683
Michael Doroff <michael_doroff@yahoo.com>
2017-09-24 21:25:27 -0500

    Removed Podfile temporarily

--- CodingWithoutStoryboard/HomeDataSourceController.swift
+++ CodingWithoutStoryboard/HomeDataSourceController.swift
@@ -8,6 +8,24 @@
 
 import LBTAComponents
 
-class HomeDataSourceController: DataSourceController {
+class HomeDatasource: Datasource {
     
+    let words = ["user1", "user2", "user3"]
+
+    override func numberOfItems(_ section: Int) -> Int {
+        return words.count
+    }
+    override func item(_ indexPath: IndexPath) -> Any? {
+        return words[indexPath.row]
+    }
+}
+
+class HomeDataSourceController: DatasourceController {
+    override func viewDidLoad() {
+       
+        
+    let homeDatasource = HomeDatasource()
+    self.datasource = homeDatasource
+        
+    }
 }
