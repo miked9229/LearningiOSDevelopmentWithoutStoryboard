9cb3fd7c20ba72fa574ffdcdc9c684b49b74e975
Michael Doroff <michael_doroff@yahoo.com>
2017-09-29 07:52:48 -0500

    Added Bio section of cell for tweet

--- CodingWithoutStoryboard/Cells.swift
+++ CodingWithoutStoryboard/Cells.swift
@@ -60,6 +60,18 @@ class UserCell: DatasourceCell {
     }()
     
     
+    let bioTextView: UITextView = {
+       let textView = UITextView()
+       textView.backgroundColor = .yellow
+      return textView
+       
+    }()
+    
+    let timePassedLabel: UILabel = {
+       let timeLabel = UILabel()
+        timeLabel.backgroundColor = .cyan
+        return timeLabel
+    }()
     
     
     override func setupViews() {
@@ -68,6 +80,8 @@ class UserCell: DatasourceCell {
         addSubview(profileImageView)
         addSubview(nameLabel)
         addSubview(usernameLabel)
+        addSubview(bioTextView)
+        addSubview(timePassedLabel)
         
         profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
         
@@ -75,6 +89,11 @@ class UserCell: DatasourceCell {
         nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: self.rightAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
         
         usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        
+        
+        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        timePassedLabel.anchor(self.topAnchor, left: nameLabel.rightAnchor, bottom: nil, right: self.rightAnchor, topConstant: 0, leftConstant: 4, bottomConstant: 0, rightConstant: 4, widthConstant: 0, heightConstant: 40)
         
         
     }
