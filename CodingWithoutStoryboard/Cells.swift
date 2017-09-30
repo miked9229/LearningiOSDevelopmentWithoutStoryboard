0ba6d2ad07a45bbeb9fddf9c6778f6bd06ae9e77
Michael Doroff <michael_doroff@yahoo.com>
2017-09-29 21:21:10 -0500

    Added constraints for follow button

--- CodingWithoutStoryboard/Cells.swift
+++ CodingWithoutStoryboard/Cells.swift
@@ -67,10 +67,10 @@ class UserCell: DatasourceCell {
        
     }()
     
-    let timePassedLabel: UILabel = {
-       let timeLabel = UILabel()
-        timeLabel.backgroundColor = .cyan
-        return timeLabel
+    let followButton: UIButton = {
+       let followButton = UIButton()
+        followButton.backgroundColor = .cyan
+        return followButton
     }()
     
     
@@ -81,19 +81,19 @@ class UserCell: DatasourceCell {
         addSubview(nameLabel)
         addSubview(usernameLabel)
         addSubview(bioTextView)
-        addSubview(timePassedLabel)
+        addSubview(followButton)
         
         profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
         
         
-        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: self.rightAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
+        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
         
         usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
         
         
         bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
         
-        timePassedLabel.anchor(self.topAnchor, left: nameLabel.rightAnchor, bottom: nil, right: self.rightAnchor, topConstant: 0, leftConstant: 4, bottomConstant: 0, rightConstant: 4, widthConstant: 0, heightConstant: 40)
+        followButton.anchor(self.topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 120, heightConstant: 34)
         
         
     }
