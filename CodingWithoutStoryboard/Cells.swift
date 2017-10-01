4612a53cb1ce27a6c9e6122e8d8af533018eb048
Michael Doroff <michael_doroff@yahoo.com>
2017-09-30 22:20:47 -0500

    Added Header Cell code

--- CodingWithoutStoryboard/Cells.swift
+++ CodingWithoutStoryboard/Cells.swift
@@ -11,13 +11,29 @@ import LBTAComponents
 
 
 class UserHeader: DatasourceCell {
+    
+    
+    let textLabel: UILabel = {
+        let label = UILabel()
+        label.text = "WHO TO FOLLOW"
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+        
+        
+        }()
+    
     override func setupViews() {
         super.setupViews()
-        backgroundColor = .blue
+        addSubview(textLabel)
+        
+    textLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
     }
 }
 
 class UserFooter: DatasourceCell {
+    
+
+    
     override func setupViews() {
         super.setupViews()
         backgroundColor = .green
