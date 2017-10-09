adf64470f47ce9aaf80add9af4a83091927fea37
Michael Doroff <michael_doroff@yahoo.com>
2017-10-08 19:22:35 -0500

    Refactored code to have setup functions in new file

--- CodingWithoutStoryboard/HomeDataSourceController.swift
+++ CodingWithoutStoryboard/HomeDataSourceController.swift
@@ -16,38 +16,10 @@ class HomeDataSourceController: DatasourceController {
     let homeDatasource = HomeDatasource()
     self.datasource = homeDatasource
     setUpNavBarItems()
-        
-    }
     
-    private func setUpNavBarItems() {
-        
-        let titleImageView = UIImageView(image: #imageLiteral(resourceName: "title_icon"))
-        titleImageView.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
-        titleImageView.contentMode = .scaleAspectFit
-        navigationItem.titleView = titleImageView
-        
-        let followButton = UIButton(type: .system)
-        followButton.setImage(#imageLiteral(resourceName: "follow").withRenderingMode(.alwaysOriginal), for: .normal)
-        followButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
-        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: followButton)
-        
-        let searchButton = UIButton(type: .system)
-        searchButton.setImage(#imageLiteral(resourceName: "search").withRenderingMode(.alwaysOriginal), for: .normal)
-        searchButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
-        
-        
-        
-        let composeButton = UIButton(type: .system)
-        composeButton.setImage(#imageLiteral(resourceName: "compose").withRenderingMode(.alwaysOriginal), for: .normal)
-        composeButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
-        
-        
-        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: composeButton), UIBarButtonItem(customView: searchButton)]
-        
-        navigationController?.navigationBar.isTranslucent = false
-        navigationController?.navigationBar.backgroundColor = .white
         
     }
+
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
@@ -90,3 +62,4 @@ class HomeDataSourceController: DatasourceController {
     
 
 }
+
