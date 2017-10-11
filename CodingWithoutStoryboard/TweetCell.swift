40876092555c452fe0ec9a9a2c9b7292427f4d47
Michael Doroff <michael_doroff@yahoo.com>
2017-10-10 21:29:15 -0500

    Added Profile Image View to Tweet Cells

--- CodingWithoutStoryboard/TweetCell.swift
+++ CodingWithoutStoryboard/TweetCell.swift
@@ -10,14 +10,35 @@ import LBTAComponents
 
 class TweetCell: DatasourceCell {
     
+    
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "profile_image")
+        imageView.layer.cornerRadius = 5
+        imageView.clipsToBounds = true
+        return imageView
+        
+    }()
+    
+    
+    
+    
+    
     override func setupViews() {
         super.setupViews()
         backgroundColor = .white
     
         separatorLineView.isHidden = false
         separatorLineView.backgroundColor = UIColor(r: 230, g: 230, b: 230)
+        
+        addSubview(profileImageView)
     
     
+        
+        profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+        
+        
     }
     
     
