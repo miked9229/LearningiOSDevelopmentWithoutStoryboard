db3cdfb102896ad7182d9d5d46b93e769c026192
Michael Doroff <michael_doroff@yahoo.com>
2017-10-25 21:13:09 -0500

    Used Generics

--- CodingWithoutStoryboard/Homedatasource.swift
+++ CodingWithoutStoryboard/Homedatasource.swift
@@ -11,6 +11,14 @@ import LBTAComponents
 import TRON
 import SwiftyJSON
 
+extension Collection where Iterator.Element == JSON {
+    func decode<T: JSONDecodable>() throws -> [T] {
+        return try map({try T(json: $0)})
+    }
+}
+
+
+
 class HomeDatasource: Datasource, JSONDecodable {
     
     let users: [User]
@@ -24,8 +32,10 @@ class HomeDatasource: Datasource, JSONDecodable {
         
         }
         
-        self.users = userJSONarray.map{User(json: $0)}
-        self.tweets = tweetsJsonArray.map({Tweet(json: $0)})
+//        self.users = userJSONarray.map{User(json: $0)}
+//        self.tweets = tweetsJsonArray.map({Tweet(json: $0)})
+        self.users = try userJSONarray.decode()
+        self.tweets = try tweetsJsonArray.decode()
         
         
     }
